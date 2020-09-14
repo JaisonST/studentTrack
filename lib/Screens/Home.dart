129e7be1bf6ab202b6026ac976114d83b8c72b4a
JaisonST <jaisonmanu@gmail.com>
2020-09-14 17:37:22 +0400

    teacherUI class added

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -9,11 +9,60 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    return TeacherUI();
+  }
+}
+
+class TeacherUI extends StatefulWidget {
+  @override
+  _TeacherUIState createState() => _TeacherUIState();
+}
+
+class _TeacherUIState extends State<TeacherUI> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text(
-        'Home',
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+
+        backgroundColor: Colors.white, //Color(0xff4DD172),
+        title: Row(
+          children: <Widget>[
+            Text(
+              'STUDENT -',
+              style: TextStyle(
+                color: Color(0xff4DD172),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '- TRACK',
+              style: TextStyle(
+                color: Colors.grey[350],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+      floatingActionButton: Container(
+        height: 70.0,
+        width: 70.0,
+        child: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            size: 40.0,
+          ),
+          backgroundColor: Color(0xff4DD172),
+          onPressed: () {},
+        ),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.yellow,
+        ),
+      ),
+    );
   }
 }
