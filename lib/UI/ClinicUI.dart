510a4b05208b5f6921f2c7b9d6b18d0c77c55b18
JaisonST <jaisonmanu@gmail.com>
2020-09-15 18:36:08 +0400

    ListView built

--- lib/UI/ClinicUI.dart
+++ lib/UI/ClinicUI.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:studenttrack/components.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ClinicUI extends StatefulWidget {
   @override
@@ -12,6 +13,17 @@ class _ClinicUIState extends State<ClinicUI> {
     return Scaffold(
       appBar: HomeAppBar(),
       floatingActionButton: ClinicAddButton(),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('Live').snapshots(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot student = snapshot.data.documents[index];
+
+                  return new Text('${student.data()["Name"]}');
+                });
+          }),
     );
   }
 }
