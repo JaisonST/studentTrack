02fd6e01d3b4ee7f2133e6f5172f6e5f16cf39d4
JaisonST <jaisonmanu@gmail.com>
2020-09-15 16:21:09 +0400

    time functionality

--- lib/DatabaseServices/Database.dart
+++ lib/DatabaseServices/Database.dart
@@ -1,19 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:time/time.dart';
 
 class DatabaseServices {
   final String uid;
   DatabaseServices({this.uid});
 
-  final CollectionReference users = FirebaseFirestore.instance.collection('Users');
-  final CollectionReference live = FirebaseFirestore.instance.collection('Live');
+  final CollectionReference users =
+      FirebaseFirestore.instance.collection('Users');
+  final CollectionReference live =
+      FirebaseFirestore.instance.collection('Live');
 
-  Future addRecordToLive(String name,String grade){
-        return live.add({
+  Future addRecordToLive(String name, String grade) {
+    return live
+        .add({
           'Name': name,
           'Class': grade,
-          'EntryTime':
-        }).then((value) => print("User Added"))
-            .catchError((error) => print("Failed to add user: $error"));
+          'EntryTime': DateTime.now().toUtc().millisecondsSinceEpoch
+        })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
   }
 
   Future<String> returnDesignation() async {
@@ -23,7 +28,4 @@ class DatabaseServices {
     });
     return returnValue;
   }
-
-
-
 }
