cb15c4795491f1f1ede161f76491bfcc316a80dc
JaisonST <jaisonmanu@gmail.com>
2020-09-15 18:57:58 +0400

    list card for clinic ui added

--- lib/UI/ClinicUI.dart
+++ lib/UI/ClinicUI.dart
@@ -21,9 +21,57 @@ class _ClinicUIState extends State<ClinicUI> {
                 itemBuilder: (context, index) {
                   DocumentSnapshot student = snapshot.data.documents[index];
 
-                  return new Text('${student.data()["Name"]}');
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xfff2f9f3),
+                      border: Border(
+                        top: BorderSide(
+                          width: 2.0,
+                          color: Color(0xff4DD172),
+                        ),
+                      ),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                    margin: EdgeInsets.only(
+                        left: 20, right: 20, bottom: 10, top: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '${student.data()["Name"]}',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  color: Color(0xff4DD172),
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${student.data()["Class"]}',
+                            ),
+                          ],
+                        ),
+                        RawMaterialButton(
+                          onPressed: () {},
+                          elevation: 1,
+                          fillColor: Colors.white,
+                          child: Icon(
+                            Icons.folder,
+                            size: 35.0,
+                            color: Colors.grey,
+                          ),
+                          padding: EdgeInsets.all(15.0),
+                          shape: CircleBorder(),
+                        )
+                      ],
+                    ),
+                  );
                 });
           }),
     );
   }
 }
+
+//Text('${student.data()["Name"]}'
