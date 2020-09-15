aaa75450350cdb0af5b272d65fda7a1af33c7f9e
JoelMathewC <joelmathewcherian@gmail.com>
2020-09-15 19:57:01 +0400

    Moving user from Live to History

--- lib/DatabaseServices/Database_Live.dart
+++ lib/DatabaseServices/Database_Live.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseLive {
   final CollectionReference live =
       FirebaseFirestore.instance.collection('Live');
+  final CollectionReference history = FirebaseFirestore.instance.collection('History');
 
   Future addRecordToLive(String name, String grade) {
     return live
@@ -10,4 +11,32 @@ class DatabaseLive {
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
   }
+
+  Future addRecordToHistory(String name,String grade, Timestamp t){
+
+    return history.add({
+      'Name': name,
+      'Class': grade,
+      'EntryTime': t,
+      'ExitTime':DateTime.now()
+    }).then((value) => print('User moved to history')).catchError((error) => print('Failed to move user:$error'));
+  }
+
+  Future moveRecordFromLiveToHistory(String uid) async {
+    String name;
+    String grade;
+    Timestamp t;
+      await live.doc(uid).get().then((DocumentSnapshot documentSnapshot){
+        name = documentSnapshot.data()['Name'];
+        grade = documentSnapshot.data()['Class'];
+        t = documentSnapshot.data()['EntryTime'];
+      });
+
+      addRecordToHistory(name, grade, t);
+
+      return live.doc(uid).delete().then((value) => print("User deleted")).catchError((error) => print("Failed to delete user: $error"));
+  }
+
+
 }
+
