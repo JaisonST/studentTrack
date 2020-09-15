159920b5845d2c36551b9081aadc77cb3538d5b0
JaisonST <jaisonmanu@gmail.com>
2020-09-15 16:00:12 +0400

    added app bar to ClinicUI

--- lib/UI/ClinicUI.dart
+++ lib/UI/ClinicUI.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:studenttrack/AuthenticationSystem/Auth.dart';
+import 'package:studenttrack/components.dart';
 
 class ClinicUI extends StatefulWidget {
   @override
@@ -10,12 +11,7 @@ class _ClinicUIState extends State<ClinicUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_back),
-        onPressed: () {
-          AuthServices().signOut(context);
-        },
-      ),
+      appBar: HomeAppBar(),
     );
   }
 }
