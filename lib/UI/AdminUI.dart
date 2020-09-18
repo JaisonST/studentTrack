79c45d7a1d45bfbc7b360609e751ba7f3d9310a2
JaisonST <jaisonmanu@gmail.com>
2020-09-18 17:04:36 +0400

    streaming students and changed icon to delete

--- lib/UI/AdminUI.dart
+++ lib/UI/AdminUI.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:studenttrack/components.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:studenttrack/DatabaseServices/Database_Live.dart';
+import 'package:studenttrack/Screens/Loading.dart';
 
 class AdminUI extends StatefulWidget {
   @override
@@ -11,10 +15,91 @@ class _AdminUIState extends State<AdminUI> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HomeAppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[],
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('Live').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return Loading();
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot student = snapshot.data.documents[index];
+
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xfff2f9f3),
+                        border: Border(
+                          top: BorderSide(
+                            width: 2.0,
+                            color: Color(0xff4DD172),
+                          ),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 20.0),
+                      margin: EdgeInsets.only(
+                          left: 20, right: 20, bottom: 10, top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                '${student.data()["Name"]}',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Color(0xff4DD172),
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '${student.data()["Class"]}',
+                              ),
+                            ],
+                          ),
+                          RawMaterialButton(
+                            onPressed: () {
+                              Alert(
+                                context: context,
+                                title: "Confirmation",
+                                desc:
+                                    "Do you want to Delete ${student.data()["Name"]}'s record",
+                                buttons: [
+                                  DialogButton(
+                                    child: Text(
+                                      'DELETE',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20.0),
+                                    ),
+                                    color: Color(0xff4DD172),
+                                    onPressed: () async {
+                                      await DatabaseLive()
+                                          .moveRecordFromLiveToHistory(
+                                              student.id)
+                                          .then((value) =>
+                                              Navigator.pop(context));
+                                    },
+                                  )
+                                ],
+                              ).show();
+                            },
+                            elevation: 1,
+                            fillColor: Colors.white,
+                            child: Icon(
+                              Icons.delete_forever,
+                              size: 35.0,
+                              color: Colors.red,
+                            ),
+                            padding: EdgeInsets.all(15.0),
+                            shape: CircleBorder(),
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            }
+          }),
     );
   }
 }
