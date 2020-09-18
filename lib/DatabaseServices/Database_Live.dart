c44dfd3e43d99ab62ab98d6bd1ad5dbc0a2be526
JaisonST <jaisonmanu@gmail.com>
2020-09-18 17:11:11 +0400

    added a delete function

--- lib/DatabaseServices/Database_Live.dart
+++ lib/DatabaseServices/Database_Live.dart
@@ -3,7 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DatabaseLive {
   final CollectionReference live =
       FirebaseFirestore.instance.collection('Live');
-  final CollectionReference history = FirebaseFirestore.instance.collection('History');
+  final CollectionReference history =
+      FirebaseFirestore.instance.collection('History');
 
   Future addRecordToLive(String name, String grade) {
     return live
@@ -12,31 +13,42 @@ class DatabaseLive {
         .catchError((error) => print("Failed to add user: $error"));
   }
 
-  Future addRecordToHistory(String name,String grade, Timestamp t){
-
-    return history.add({
-      'Name': name,
-      'Class': grade,
-      'EntryTime': t,
-      'ExitTime':DateTime.now()
-    }).then((value) => print('User moved to history')).catchError((error) => print('Failed to move user:$error'));
+  Future addRecordToHistory(String name, String grade, Timestamp t) {
+    return history
+        .add({
+          'Name': name,
+          'Class': grade,
+          'EntryTime': t,
+          'ExitTime': DateTime.now()
+        })
+        .then((value) => print('User moved to history'))
+        .catchError((error) => print('Failed to move user:$error'));
   }
 
   Future moveRecordFromLiveToHistory(String uid) async {
     String name;
     String grade;
     Timestamp t;
-      await live.doc(uid).get().then((DocumentSnapshot documentSnapshot){
-        name = documentSnapshot.data()['Name'];
-        grade = documentSnapshot.data()['Class'];
-        t = documentSnapshot.data()['EntryTime'];
-      });
+    await live.doc(uid).get().then((DocumentSnapshot documentSnapshot) {
+      name = documentSnapshot.data()['Name'];
+      grade = documentSnapshot.data()['Class'];
+      t = documentSnapshot.data()['EntryTime'];
+    });
 
-      addRecordToHistory(name, grade, t);
+    addRecordToHistory(name, grade, t);
 
-      return live.doc(uid).delete().then((value) => print("User deleted")).catchError((error) => print("Failed to delete user: $error"));
+    return live
+        .doc(uid)
+        .delete()
+        .then((value) => print("User deleted"))
+        .catchError((error) => print("Failed to delete user: $error"));
   }
 
-
+  deleteRecord(String uid) async {
+    await live
+        .doc(uid)
+        .delete()
+        .then((value) => print("User deleted"))
+        .catchError((error) => print("Failed to delete user: $error"));
+  }
 }
-
