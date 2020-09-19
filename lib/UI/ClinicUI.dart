eb63a5b3f9e0ce5e3709bffb7f4349f9060d0e46
JaisonST <jaisonmanu@gmail.com>
2020-09-19 13:04:10 +0400

    edited clinicUi added no cases UI

--- lib/UI/ClinicUI.dart
+++ lib/UI/ClinicUI.dart
@@ -22,87 +22,95 @@ class _ClinicUIState extends State<ClinicUI> {
             if (snapshot.data == null) {
               return Loading();
             } else {
-              return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot student = snapshot.data.documents[index];
+              if (snapshot.data.documents.length == 0) {
+                return Center(
+                  child: Text(
+                    'There are no Cases right now',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot student = snapshot.data.documents[index];
 
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xfff2f9f3),
-                        border: Border(
-                          top: BorderSide(
-                            width: 2.0,
-                            color: Color(0xff4DD172),
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xfff2f9f3),
+                          border: Border(
+                            top: BorderSide(
+                              width: 2.0,
+                              color: Color(0xff4DD172),
+                            ),
                           ),
                         ),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                          horizontal: 10.0, vertical: 20.0),
-                      margin: EdgeInsets.only(
-                          left: 20, right: 20, bottom: 10, top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                '${student.data()["Name"]}',
-                                style: TextStyle(
-                                    fontSize: 20.0,
-                                    color: Color(0xff4DD172),
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                '${student.data()["Class"]}',
-                              ),
-                            ],
-                          ),
-                          RawMaterialButton(
-                            onPressed: () {
-                              Alert(
-                                context: context,
-                                title: "Confirmation",
-                                desc:
-                                    "Do you want to Archive ${student.data()["Name"]}'s record",
-                                buttons: [
-                                  DialogButton(
-                                    child: Text(
-                                      'ARCHIVE',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20.0),
-                                    ),
-                                    color: Color(0xff4DD172),
-                                    onPressed: () async {
-                                      await DatabaseLive()
-                                          .moveRecordFromLiveToHistory(
-                                              student.id)
-                                          .then((value) =>
-                                              Navigator.pop(context));
-                                    },
-                                  )
-                                ],
-                              ).show();
-                            },
-                            elevation: 1,
-                            fillColor: Colors.white,
-                            child: Icon(
-                              Icons.folder,
-                              size: 35.0,
-                              color: Colors.grey,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 20.0),
+                        margin: EdgeInsets.only(
+                            left: 20, right: 20, bottom: 10, top: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  '${student.data()["Name"]}',
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      color: Color(0xff4DD172),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  '${student.data()["Class"]}',
+                                ),
+                              ],
                             ),
-                            padding: EdgeInsets.all(15.0),
-                            shape: CircleBorder(),
-                          )
-                        ],
-                      ),
-                    );
-                  });
+                            RawMaterialButton(
+                              onPressed: () {
+                                Alert(
+                                  context: context,
+                                  title: "Confirmation",
+                                  desc:
+                                      "Do you want to Archive ${student.data()["Name"]}'s record",
+                                  buttons: [
+                                    DialogButton(
+                                      child: Text(
+                                        'ARCHIVE',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20.0),
+                                      ),
+                                      color: Color(0xff4DD172),
+                                      onPressed: () async {
+                                        await DatabaseLive()
+                                            .moveRecordFromLiveToHistory(
+                                                student.id)
+                                            .then((value) =>
+                                                Navigator.pop(context));
+                                      },
+                                    )
+                                  ],
+                                ).show();
+                              },
+                              elevation: 1,
+                              fillColor: Colors.white,
+                              child: Icon(
+                                Icons.folder,
+                                size: 35.0,
+                                color: Colors.grey,
+                              ),
+                              padding: EdgeInsets.all(15.0),
+                              shape: CircleBorder(),
+                            )
+                          ],
+                        ),
+                      );
+                    });
+              }
             }
           }),
     );
   }
 }
-
-//Text('${student.data()["Name"]}'
