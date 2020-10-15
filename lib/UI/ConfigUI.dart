c502032e63437b704938df2c091a4caeda6f84ff
JaisonST <jaisonmanu@gmail.com>
2020-10-16 01:44:13 +0400

    Config buttons added

--- lib/UI/ConfigUI.dart
+++ lib/UI/ConfigUI.dart
@@ -19,6 +19,66 @@ class _ConfigUIState extends State<ConfigUI> {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: SizedBox(),
+          ),
+          Expanded(
+            flex: 2,
+            child: ConfigChoice(display: "Edit Emergency Emails"),
+          ),
+          Expanded(
+            flex: 2,
+            child: ConfigChoice(
+              display: "Set Up Washrooms",
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: SizedBox(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ConfigChoice extends StatelessWidget {
+  final String display;
+  ConfigChoice({@required this.display});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialButton(
+        onPressed: () {},
+        child: Row(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Text(
+                display,
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            Expanded(
+              flex: 1,
+              child: Icon(Icons.build, color: Colors.white),
+            )
+          ],
+        ),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color(0xff4DD172),
+      ),
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
     );
   }
 }
