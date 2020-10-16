867c28b9a0c88758069400956d8e4bcd34e770f1
JaisonST <jaisonmanu@gmail.com>
2020-10-16 22:04:24 +0400

    Removed Login AppBar

--- lib/Screens/LogIn.dart
+++ lib/Screens/LogIn.dart
@@ -22,111 +22,84 @@ class _LogInState extends State<LogIn> {
     return loading
         ? Loading()
         : Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              titleSpacing: 0.0,
-              leading: FlatButton(
-                child: Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop(context),
-              ),
-              iconTheme: IconThemeData(
-                  color: Color(0xff696969), size: 30.0 //change your color here
-                  ),
-              title: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Student',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30.0,
-                          color: Color(0xff4dd172),
-                        )),
-                    Text('Track',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30.0,
-                          color: Color(0xff696969),
-                        ))
-                  ]),
-            ),
             body: Padding(
-              padding: const EdgeInsets.fromLTRB(40.0, 50.0, 30.0, 0.0),
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 40.0),
-                      Flexible(child: StudentTrackTitle()),
-                      SizedBox(height: 60.0),
-                      TextFormField(
-                          decoration: InputDecoration(
-                              hintText: 'Email Address',
-                              enabledBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.white, width: 2.0),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                color: Color(0xff696969),
-                                width: 2.0,
-                              ))),
-                          validator: (val) =>
-                              val.isEmpty ? "Enter a valid id" : null,
-                          onChanged: (val) {
-                            setState(() => email = val);
-                          }),
-                      SizedBox(height: 10.0),
-                      TextFormField(
-                          decoration: InputDecoration(
-                              hintText: 'Password',
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Colors.white, width: 2.0)),
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                      color: Color(0xff696969), width: 2.0))),
-                          validator: (val) =>
-                              val.length < 6 ? 'Password not Valid' : null,
-                          obscureText: true,
-                          onChanged: (val) {
-                            setState(() => password = val);
-                          }),
-                      SizedBox(height: 30.0),
-                      SizedBox(
-                        height: 50.0,
-                        width: 100.0,
-                        child: RaisedButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Color(0xff4dd172))),
-                            onPressed: () async {
-                              setState(() {
-                                loading = true;
-                              });
-                              if (_formKey.currentState.validate()) {
-                                dynamic result = await _auth.signIn(
-                                    email, password, context);
-                                if (result == null) {
-                                  setState(() {
-                                    loading = false;
-                                    error =
-                                        'Please enter a valid Email Id and corresponding Password';
-                                  });
-                                }
+            padding: const EdgeInsets.fromLTRB(40.0, 50.0, 30.0, 20.0),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40.0),
+                    Flexible(child: StudentTrackTitle()),
+                    SizedBox(height: 60.0),
+                    TextFormField(
+                        decoration: InputDecoration(
+                            hintText: 'Email Address',
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                              color: Color(0xff696969),
+                              width: 2.0,
+                            ))),
+                        validator: (val) =>
+                            val.isEmpty ? "Enter a valid id" : null,
+                        onChanged: (val) {
+                          setState(() => email = val);
+                        }),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                        decoration: InputDecoration(
+                            hintText: 'Password',
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white, width: 2.0)),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xff696969), width: 2.0))),
+                        validator: (val) =>
+                            val.length < 6 ? 'Password not Valid' : null,
+                        obscureText: true,
+                        onChanged: (val) {
+                          setState(() => password = val);
+                        }),
+                    SizedBox(height: 30.0),
+                    SizedBox(
+                      height: 50.0,
+                      width: 100.0,
+                      child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Color(0xff4dd172))),
+                          onPressed: () async {
+                            setState(() {
+                              loading = true;
+                            });
+                            if (_formKey.currentState.validate()) {
+                              dynamic result =
+                                  await _auth.signIn(email, password, context);
+                              if (result == null) {
+                                setState(() {
+                                  loading = false;
+                                  error =
+                                      'Please enter a valid Email Id and corresponding Password';
+                                });
                               }
-                            },
-                            child: Text('Sign In',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16.0)),
-                            color: Color(0xff4dd172)),
-                      ),
-                      SizedBox(height: 20.0),
-                      Text(error,
-                          style: TextStyle(
-                              color: Colors.black, fontStyle: FontStyle.italic))
-                    ],
-                  )),
-            ));
+                            }
+                          },
+                          child: Text('Sign In',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 16.0)),
+                          color: Color(0xff4dd172)),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(error,
+                        style: TextStyle(
+                            color: Colors.black, fontStyle: FontStyle.italic))
+                  ],
+                )),
+          ));
   }
 }
 
