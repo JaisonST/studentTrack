e68c18a9d0e38adef9ad1eeac2cd3e48950c42ad
JaisonST <jaisonmanu@gmail.com>
2020-11-23 00:10:37 +0400

    scan page ui edits

--- lib/ScanPage.dart
+++ lib/ScanPage.dart
@@ -13,7 +13,12 @@ class ScanPage extends StatefulWidget {
   String schoolDB;
   String collectionName;
 
-  ScanPage({@required this.localDesc,@required this.collectionName,@required this.localColor,@required this.localTitle,@required this.schoolDB});
+  ScanPage(
+      {@required this.localDesc,
+      @required this.collectionName,
+      @required this.localColor,
+      @required this.localTitle,
+      @required this.schoolDB});
   @override
   _ScanPageState createState() => _ScanPageState();
 }
@@ -22,12 +27,11 @@ class _ScanPageState extends State<ScanPage> {
   String _qrInfo = '';
   bool _camState = false;
 
-  _qrCallback(String code){
-    setState((){
+  _qrCallback(String code) {
+    setState(() {
       _camState = false;
       _qrInfo = code;
     });
-
   }
 
   _scanCode() {
@@ -49,111 +53,110 @@ class _ScanPageState extends State<ScanPage> {
 
   Widget _cameraControlWidget(context) {
     return Expanded(
-        child: Align(
-            alignment: Alignment.center,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text(
-                    'Enter Details Manually',
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  color: Color(0xff4DD172),
-                  onPressed: () async {
-
-                    await clinicForm(context, widget.localTitle, widget.localDesc, widget.localColor, widget.schoolDB, widget.collectionName);
-
-                  },
-                )
-              ],
-            )));
+        child: RaisedButton(
+      child: Text(
+        'Enter Details Manually',
+        style: TextStyle(
+            fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      color: Color(0xff4DD172),
+      onPressed: () async {
+        await clinicForm(context, widget.localTitle, widget.localDesc,
+            widget.localColor, widget.schoolDB, widget.collectionName);
+      },
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-
     bool found = false;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.localTitle),
-      ),
-      body: _camState
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                  Expanded(
-                    flex: 5,
-                    child: Center(
-                      child: SizedBox(
-                        height: 700,
-                        width: 500,
-                        child: QRBarScannerCamera(
-                          onError: (context, error) => Text(
-                            error.toString(),
-                            style: TextStyle(color: Colors.red),
-                          ),
-                          qrCodeCallback: (code) async {
-                            _qrCallback(code);
-                            found = await DatabaseLive(schoolDB: widget.schoolDB, collectionName: widget.collectionName).scanAddRecordToLive(code);
-                            print('Found:');
-                            print(found);
-                            if(found) {
-                              await Alert(
-                                context: context,
-                                title: 'Success',
-                                buttons: [
-                                  DialogButton(color: Colors.green,
-                                      child: Text('done'),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      })
-                                ],
-                                style: AlertStyle(
-                                    titleStyle: TextStyle(color: Colors.green)),
-                              ).show().then((value) => Navigator.pop(context));
-                            }
-                            else{
-                              await Alert(
-                                context: context,
-                                title: 'Could not find Student record',
-                                buttons: [DialogButton(color: Colors.red, child: Text('Try Again'), onPressed:(){Navigator.pop(context);})],
-                                style: AlertStyle(titleStyle: TextStyle(color: Colors.red)),
-                              ).show();
-                              setState(() {
-                                _camState = true;
-                              });
-                            }
-                          },
+        appBar: AppBar(
+          backgroundColor: Color(0xff4DD172),
+          title: Text(widget.localTitle),
+        ),
+        body: _camState
+            ? Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <
+                Widget>[
+                Expanded(
+                  flex: 5,
+                  child: Center(
+                    child: SizedBox(
+                      height: 700,
+                      width: 500,
+                      child: QRBarScannerCamera(
+                        onError: (context, error) => Text(
+                          error.toString(),
+                          style: TextStyle(color: Colors.red),
                         ),
+                        qrCodeCallback: (code) async {
+                          _qrCallback(code);
+                          found = await DatabaseLive(
+                                  schoolDB: widget.schoolDB,
+                                  collectionName: widget.collectionName)
+                              .scanAddRecordToLive(code);
+                          print('Found:');
+                          print(found);
+                          if (found) {
+                            await Alert(
+                              context: context,
+                              title: 'Success',
+                              buttons: [
+                                DialogButton(
+                                    color: Color(0xff4DD172),
+                                    child: Text(
+                                      'Done',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    })
+                              ],
+                              style: AlertStyle(
+                                  titleStyle: TextStyle(color: Colors.black)),
+                            ).show().then((value) => Navigator.pop(context));
+                          } else {
+                            await Alert(
+                              context: context,
+                              title: 'Could not find Student record',
+                              buttons: [
+                                DialogButton(
+                                    color: Colors.red,
+                                    child: Text(
+                                      'Try Again',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    })
+                              ],
+                              style: AlertStyle(
+                                  titleStyle: TextStyle(color: Colors.black)),
+                            ).show();
+                            setState(() {
+                              _camState = true;
+                            });
+                          }
+                        },
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        height: 120,
-                        width: double.infinity,
-                        padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
-                        color: Colors.white,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            _cameraControlWidget(context),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
-                ])
-          : Loading()
-    );
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
+                    color: Colors.white,
+                    child: _cameraControlWidget(context),
+                  ),
+                )
+              ])
+            : Loading());
   }
 }
